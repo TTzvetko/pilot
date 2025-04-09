7ec63e08f2aa50e1425ce14e5de538ef5ea5a9ba
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:42:34 +0300

    Update nervous.js

--- pages/systems/nervous.js
+++ pages/systems/nervous.js
@@ -1,6 +1,6 @@
 export default function Nervous() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Nervous System</h1>
       <p>This page will contain information and supplements related to the nervous system.</p>
     </div>
