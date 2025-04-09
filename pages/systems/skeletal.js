4c4f6be9736a4a40207b869d9e1feb5860d8801b
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:43:01 +0300

    Update skeletal.js

--- pages/systems/skeletal.js
+++ pages/systems/skeletal.js
@@ -1,6 +1,6 @@
 export default function Skeletal() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Skeletal System</h1>
       <p>This page will contain information and supplements related to the skeletal system.</p>
     </div>
