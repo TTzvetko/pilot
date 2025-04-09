3087262b0bce00756bc80133de84a52b3b1d23cb
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:41:35 +0300

    Update female-reproductive.js

--- pages/systems/female-reproductive.js
+++ pages/systems/female-reproductive.js
@@ -1,6 +1,6 @@
 export default function FemaleReproductive() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Female Reproductive System</h1>
       <p>This page will contain information and supplements related to the female reproductive system.</p>
     </div>
