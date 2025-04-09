41811eb0a09472efa2c619a7fed398692b50d97a
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:41:07 +0300

    Update digestive.js

--- pages/systems/digestive.js
+++ pages/systems/digestive.js
@@ -1,6 +1,6 @@
 export default function Digestive() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Digestive System</h1>
       <p>This page will contain information and supplements related to the digestive system.</p>
     </div>
