c071ee5cbf2d13e40205a4884eb371cde4f99113
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:41:49 +0300

    Update immune.js

--- pages/systems/immune.js
+++ pages/systems/immune.js
@@ -1,6 +1,6 @@
 export default function Immune() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Immune System</h1>
       <p>This page will contain information and supplements related to the immune system.</p>
     </div>
