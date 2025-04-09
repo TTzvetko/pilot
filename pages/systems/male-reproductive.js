2fba87613aec8db777cf79fead835d172161c88e
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:42:03 +0300

    Update male-reproductive.js

--- pages/systems/male-reproductive.js
+++ pages/systems/male-reproductive.js
@@ -1,6 +1,6 @@
 export default function MaleReproductive() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Male Reproductive System</h1>
       <p>This page will contain information and supplements related to the male reproductive system.</p>
     </div>
