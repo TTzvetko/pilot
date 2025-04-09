d0acfb7e3f2bee580bcf17598085a658322483d8
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:41:22 +0300

    Update endocrine.js

--- pages/systems/endocrine.js
+++ pages/systems/endocrine.js
@@ -1,6 +1,6 @@
 export default function Endocrine() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Endocrine System</h1>
       <p>This page will contain information and supplements related to the endocrine system.</p>
     </div>
