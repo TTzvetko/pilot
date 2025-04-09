0ac2bc32e4a911b03c3265308344ebb6c9a3bbb1
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:43:14 +0300

    Update urinary.js

--- pages/systems/urinary.js
+++ pages/systems/urinary.js
@@ -1,6 +1,6 @@
 export default function Urinary() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Urinary System</h1>
       <p>This page will contain information and supplements related to the urinary system.</p>
     </div>
