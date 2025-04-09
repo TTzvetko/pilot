3ebbe28cabfdb3b962c7ddedbbbbfa834ad72e2c
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:42:48 +0300

    Update respiratory.js

--- pages/systems/respiratory.js
+++ pages/systems/respiratory.js
@@ -1,6 +1,6 @@
 export default function Respiratory() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Respiratory System</h1>
       <p>This page will contain information and supplements related to the respiratory system.</p>
     </div>
