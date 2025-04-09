11d25dfd901c6e564b132d35324e23d625844ab4
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:40:47 +0300

    Update cardiovascular.js

--- pages/systems/cardiovascular.js
+++ pages/systems/cardiovascular.js
@@ -1,6 +1,6 @@
 export default function Cardiovascular() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Cardiovascular System</h1>
       <p>This page will contain information and supplements related to the cardiovascular system.</p>
     </div>
