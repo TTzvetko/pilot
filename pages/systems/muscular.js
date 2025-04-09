8f1b832d66ea64726eb78d82df0b61aee1ec2d21
TTzvetko <tzvetkov.teodor@gmail.com>
2025-04-09 10:42:17 +0300

    Update muscular.js

--- pages/systems/muscular.js
+++ pages/systems/muscular.js
@@ -1,6 +1,6 @@
 export default function Muscular() {
   return (
-    <div style={ padding: '40px', fontFamily: 'sans-serif' }>
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <h1>Muscular System</h1>
       <p>This page will contain information and supplements related to the muscular system.</p>
     </div>
